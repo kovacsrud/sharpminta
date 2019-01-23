fb0ed057f43c0f9ad5d9c6af85fd174fd1769621
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-01-23 16:48:33 +0100

    #2
    A osztály létrehozása

---
+++ GitDemo/GitDemo/A.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tanar
+ * Date: 2019. 01. 23.
+ * Time: 16:48
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace GitDemo
+{
+	/// <summary>
+	/// Description of A.
+	/// </summary>
+	public class A
+	{
+		public A()
+		{
+		}
+	}
+}
