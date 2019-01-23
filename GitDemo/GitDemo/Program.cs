86b47deab8f4347bdc7b61c47080fd1a6cf18d6b
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-01-23 16:38:42 +0100

    Program létrehozása
    Projekt start

---
+++ GitDemo/GitDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tanar
+ * Date: 2019. 01. 23.
+ * Time: 16:37
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace GitDemo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.ReadLine();
+		}
+	}
+}
